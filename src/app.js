948edb975d249b566cce8170a3199f0c02a6c058
Yasmine99college <yasminaahmedabcd@gmail.com>
2023-06-05 18:59:16 +0300

    Edit

--- src/app.js
+++ src/app.js
@@ -1,117 +1,74 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://user1:PENCILSPACE@atlascluster.ehnugvt.mongodb.net/?retryWrites=true&w=majority'
-    ,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-).then(() => {
-    console.log('Connected to MongoDB');
-})
-    .catch((error) => {
+async function connectToMongoDB() {
+    try {
+
+        await mongoose.connect('mongodb+srv://user1:123@atlascluster.ehnugvt.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
         console.error('Error connecting to MongoDB:', error);
-    });
+    }
+}
+
+connectToMongoDB();
 const Question = require('./question');
 const Topic = require('./topics');
 
+
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
+app.get('/search', async (req, res) => {
+    try {
+        const { q } = req.query;
+        const topics = await Topic.find().exec();
 
-
-
-
-app.get('/search', (req, res) => {
-    const { q } = req.query; 
-
-    Topic.find()
-        .then((topics) => {
-            // Find the matching topic
-            const matchingTopics = topics.filter((topic) => {
-                const level1Match = topic['Topic Level 1'] && topic['Topic Level 1'].includes(q);
-                const level2Match = topic['Topic Level 2'] && topic['Topic Level 2'].includes(q);
-                const level3Match = topic['Topic Level 3'] && topic['Topic Level 3'].includes(q);
-                return level1Match || level2Match || level3Match;
-            });
-
-            if (matchingTopics.length > 0) {
-                const matchQuery = {};
-                for (let i = 1; i <= 5; i++) {
-                    matchQuery[`Annotation ${i}`] = {
-                        $in: matchingTopics.flatMap((topic) => [
-                            topic['Topic Level 1'],
-                            topic['Topic Level 2'],
-                            topic['Topic Level 3'],
-                        ]).filter((value) => value !== undefined),
-                    };
-                }
-                // const sampleData = [
-                //     {
-                //         'Question number': 1,
-                //         'Annotation 1': 'Sample annotation 1',
-                //         'Annotation 2': 'Sample annotation 2',
-                //         'Annotation 3': 'Sample annotation 3',
-                //         'Annotation 4': 'Sample annotation 4',
-                //         'Annotation 5': 'Sample annotation 5',
-                //     },
-                //     // Add more sample documents here
-                // ];
-
-                // Question.create(sampleData)
-                //     .then(() => {
-                //         console.log('Sample data inserted successfully');
-                //         // Proceed with executing the search code
-                //     })
-                //     .catch((error) => {
-                //         console.error('Error inserting sample data:', error);
-                //     });
-                // Question.find({}, 'Question number')
-                //     .then((questions) => {
-                //         const questionNumbers = questions.map((question) => question['Question number']);
-                //         console.log(questionNumbers);
-                //     })
-                //     .catch((error) => {
-                //         console.error('Error finding question numbers:', error);
-                //     });
-
-
-
-                console.log(matchQuery);
-                Question.find({}, 'Annotation 1')
-                    .then((questions) => {
-                        const questionNumbers = questions.map((question) => question['Annotation 1']);
-                        console.log(questionNumbers);
-                    })
-                    .catch((error) => {
-                        console.error('Error finding question numbers:', error);
-                    });
-                
-                Question.find(matchQuery)
-                    .then( 
-                        (questions) => {
-                        console.log(questions);
-                        const questionNumbers = questions.map((question) => question['Question number']);
-                        res.json(questionNumbers);
-                    })
-                    .catch((error) => {
-                        console.error('Error finding questions:', error);
-                        res.status(500).json({ error: 'Internal Server Error' });
-                    });
-            } else {
-                res.json([]); 
-            }
-        })
-        .catch((error) => {
-            console.error('Error finding topics:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
+        const matchingTopics = topics.filter((topic) => {
+            const level1Match = topic['Topic Level 1'] && topic['Topic Level 1'].includes(q);
+            const level2Match = topic['Topic Level 2'] && topic['Topic Level 2'].includes(q);
+            const level3Match = topic['Topic Level 3'] && topic['Topic Level 3'].includes(q);
+            return level1Match || level2Match || level3Match;
         });
+        console.log(matchingTopics);
+
+        let questionNumbers = [];
+
+        for (let i = 1; i <= 5; i++) {
+            const annotationKey = `Annotation ${i}`;
+            const annotations = matchingTopics
+                .flatMap((topic) => [
+                    topic['Topic Level 1'],
+                    topic['Topic Level 2'],
+                    topic['Topic Level 3'],
+                ])
+                .filter((value) => value !== undefined);
+
+            if (annotations.length > 0) {
+                const matchQuery = { [annotationKey]: { $in: annotations } };
+                const questions = await Question.find(matchQuery).exec();
+                questionNumbers.push(questions.map((question) => question['Question number']));
+            }
+        }
+        let newArr = [];
+        for (var i = 0; i < questionNumbers.length; i++) {
+            newArr = newArr.concat(questionNumbers[i]);
+        }
+
+        console.log('Question Numbers:', newArr);
+        res.json(newArr);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
-
-
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
